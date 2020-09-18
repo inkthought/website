19e3fb7f753e3030db9dc28f3d1193acd6a49575
flarely <flarely@users.noreply.github.com>
2020-09-18 18:58:11 +0800

    ✨ Getting ready for JavaScript implementation

--- src/webscript.js
+++ src/webscript.js
@@ -1,1 +1,9 @@
-/* coming soon */
+/* document.getElementById("h31").innerHTML = 
+"welcome to <em>inkthought labs</em>.";
+
+document.getElementById("h41").innerHTML = 
+"<em>currently in development...</em>"
+
+document.getElementById("h21").innerHTML = 
+"honkers, a discord goose"
+*/

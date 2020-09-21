d491d178a6e6df8c33d9db35c0c0ccee2a7cad59
flarely <flarely@users.noreply.github.com>
2020-09-21 18:15:45 +0800

    🥚 heyyoufoundme easter egg!

--- src/webscript.js
+++
@@ -1,9 +0,0 @@
-/* document.getElementById("h31").innerHTML = 
-"welcome to <em>inkthought labs</em>.";
-
-document.getElementById("h41").innerHTML = 
-"<em>currently in development...</em>"
-
-document.getElementById("h21").innerHTML = 
-"honkers, a discord goose"
-*/
